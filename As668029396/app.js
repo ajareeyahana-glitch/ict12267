75c633af088703ee29c8ac2cd72ba9cc67508e5b
ajareeyahana-glitch <ajareeya.hana@gmail.com>
2026-02-25 00:42:50 +0700

    Add files via upload

---
+++ As668029396/app.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById("darkModeBtn");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+const btn = document.getElementById("darkBtn");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    btn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
+
+// Scroll animation
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(sec => {
+        const top = sec.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            sec.classList.add("show");
+        }
+    });
+});
